e79b80c131ce0bde22ad3fed17b8785896bae814
Matheus Gabriel de Oliveira Rodrigues <76976847+matheus-mg@users.noreply.github.com>
2025-02-13 14:54:36 -0300

    Update Teste-Portal.spec.js

--- cypress/integration/Teste-Portal.spec.js
+++ cypress/integration/Teste-Portal.spec.js
@@ -2,11 +2,11 @@ describe('Testar login como usuári', function() {
     beforeEach(function(){
         cy.visit('https://portal-antecipacao-teste.profrotas.com.br/#!/login?r=true')
 
-
+})
     it('Testa o login no portal' function() {
       cy.get('#username').type('teste')
 
-    })
+})
 
 })
 
